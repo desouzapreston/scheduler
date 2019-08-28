3b619faedd34c42429420d8d4a8e225b06968873
Preston deSouza <desouzapreston@gmail.com>
2019-08-28 09:57:53 -0500

    Changing from Calendar API to FormControl Layout

--- scheduler/src/app/app.module.ts
+++ scheduler/src/app/app.module.ts
@@ -4,24 +4,17 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CalendarModule, DateAdapter } from 'angular-calendar';
-import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
-import { EventCalendarComponent } from './event-calendar/event-calendar.component';
+// TODO: Write a lot more components/Testing gitTree
 
 @NgModule({
   declarations: [
     AppComponent,
-    EventCalendarComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
-    CalendarModule.forRoot({
-      provide: DateAdapter,
-      useFactory: adapterFactory
-    }),
   ],
   providers:[],
   bootstrap: [AppComponent]

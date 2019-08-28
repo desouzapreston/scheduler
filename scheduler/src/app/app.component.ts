201283dad21cf7ae85f7a2b998d7173e7a8d55e8
Preston deSouza <desouzapreston@gmail.com>
2019-08-28 12:33:20 -0500

    Implementation of DateTimePicker

--- scheduler/src/app/app.component.ts
+++ scheduler/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators, AbstractControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'scheduler';
+  formGroup: FormGroup;
+  dateModel: Date = new Date();
+
+  stringDateModel: string = new Date().toString();
+
+  ngOnInit() {
+    this.formGroup = new FormGroup({
+      activeEndDate: new FormControl(new Date(), { validators: [Validators.required, DateTimeValidator] })
+    }, { updateOn: 'change' });
+  }
 }
+
+export const DateTimeValidator = (fc: FormControl) => {
+  const date = new Date(fc.value);
+  const isValid = !isNaN(date.valueOf());
+  return isValid ? null : {
+    isValid: {
+      valid: false
+    }
+  };
+};
--- scheduler/src/app/app.module.ts
+++ scheduler/src/app/app.module.ts
@@ -1,9 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { library as fontLibrary } from '@fortawesome/fontawesome-svg-core';
+import { faCalendar,  faClock } from '@fortawesome/free-regular-svg-icons';
+
 // TODO: Write a lot more components/Testing gitTree
 
 @NgModule({

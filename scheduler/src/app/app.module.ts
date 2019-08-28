1d9e821a5f289a900a801311d5642a9353653b66
Preston deSouza <desouzapreston@gmail.com>
2019-08-28 13:34:29 -0500

    Re-Doing Styling

--- scheduler/src/app/app.module.ts
+++ scheduler/src/app/app.module.ts
@@ -4,22 +4,37 @@ import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library as fontLibrary } from '@fortawesome/fontawesome-svg-core';
-import { faCalendar,  faClock } from '@fortawesome/free-regular-svg-icons';
+import { EventCalendarComponent } from './event-calendar/event-calendar.component';
+import { EventDisplayComponent } from './event-display/event-display.component';
+import { MatRippleModule, MatButtonModule, MatFormFieldModule, MatInputModule } from '@angular/material';
+// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+// import { library as fontLibrary } from '@fortawesome/fontawesome-svg-core';
+// import { faCalendar,  faClock } from '@fortawesome/free-regular-svg-icons';
 
 // TODO: Write a lot more components/Testing gitTree
-
+const modules = [
+  MatButtonModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRippleModule
+];
 @NgModule({
   declarations: [
     AppComponent,
+    EventCalendarComponent,
+    EventDisplayComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
     FormsModule,
+    [...modules],
+
+  ],
+  exports: [
+    [...modules],
   ],
   providers:[],
   bootstrap: [AppComponent]
